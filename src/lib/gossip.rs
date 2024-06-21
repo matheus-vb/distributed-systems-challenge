c3025849499364dee1e91c97bb739aa83c9f7d4c
Matheus Batista <matheus.vbatista04@gmail.com>
2024-06-21 02:12:14 -0300

    add gossip protocol to reduce latency

--- src/lib/gossip.rs
+++ src/lib/gossip.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Write};
 
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
 
 use crate::message::{AppState, Body, Message, PayloadType, SeenMessage};
@@ -12,6 +14,8 @@ pub struct GossipPayload {
     pub id: String,
 }
 
+// When a message arrives, the node evaluates if it is a new one
+// If it is a new message, the node will randomly select a subset of nodes to send the message to.
 impl GossipPayload {
     pub fn handle(
         &self,
@@ -26,25 +30,37 @@ impl GossipPayload {
 
         let src_id = &app_state.src_id.clone().unwrap();
 
-        if let Some(nodes) = app_state.neighbours.get(src_id.as_str()) {
-            for node in nodes {
-                let new_message = Message {
-                    src: src_id.to_string(),
-                    dest: node.to_string(),
-                    body: Body {
-                        payload: PayloadType::Gossip(GossipPayload {
-                            message: self.message.clone(),
-                            id: self.id.clone(),
-                        }),
-                        msg_id: message.body.msg_id,
-                        in_reply_to: message.body.msg_id,
-                    },
-                };
+        let all_nodes: Vec<String> = app_state
+            .neighbours
+            .values()
+            .flat_map(|n| n.iter().cloned())
+            .collect();
 
-                let output = serde_json::to_string(&new_message).unwrap();
-                writer.write_all(output.as_bytes())?;
-                writer.write_all(b"\n")?;
-            }
+        let num_gossip = all_nodes.len() / 5;
+
+        let mut rng = thread_rng();
+        let mut subset = all_nodes.clone();
+
+        subset.shuffle(&mut rng);
+        subset.truncate(num_gossip);
+
+        for node in subset {
+            let new_message = Message {
+                src: src_id.to_string(),
+                dest: node.to_string(),
+                body: Body {
+                    payload: PayloadType::Gossip(GossipPayload {
+                        message: self.message.clone(),
+                        id: self.id.clone(),
+                    }),
+                    msg_id: message.body.msg_id,
+                    in_reply_to: message.body.msg_id,
+                },
+            };
+
+            let output = serde_json::to_string(&new_message).unwrap();
+            writer.write_all(output.as_bytes())?;
+            writer.write_all(b"\n")?;
         }
 
         app_state.record.insert(self.id.clone(), self.message);
